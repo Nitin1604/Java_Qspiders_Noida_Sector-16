6e9144b6216892fe72f7ef949505e5e9e7f9fc83
Nitin <nitinsokhal@gmail.com>
2025-01-19 00:07:39 +0530

    Java Qspiders Questions

--- java 12pm/Constructor/Default/CarDriver.java
+++
@@ -1,7 +0,0 @@
-class CarDriver{
-	public static void main(String[] args){
-		System.out.println("This is carDriver class");
-		Car c1=new Car();
-	}
-}
-
--- java 12pm/Constructor/No argument/Car.java
+++
@@ -1,9 +0,0 @@
-class Car{
-	static int count=0;
-	Car(){
-		count++;
-		System.out.println("Car Object "+count+" is created");
-	}
-	
-}
-
